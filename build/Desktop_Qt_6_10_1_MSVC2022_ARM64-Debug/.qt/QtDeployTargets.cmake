4c0e30cc0b11559703b191c9f2923487d3574b5a
celine <celine.choln@gmail.com>
2026-01-26 21:41:39 +0100

    TP1QT2

---
+++ build/Desktop_Qt_6_10_1_MSVC2022_ARM64-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_TP2_QT_2_FILE C:/Users/celin/Documents/TP2_QT_2/build/Desktop_Qt_6_10_1_MSVC2022_ARM64-Debug/TP2_QT_2.exe)
+set(__QT_DEPLOY_TARGET_TP2_QT_2_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_TP2_QT_2_RUNTIME_DLLS C:/Qt/6.10.1/msvc2022_arm64/bin/Qt6Widgetsd.dll;C:/Qt/6.10.1/msvc2022_arm64/bin/Qt6Guid.dll;C:/Qt/6.10.1/msvc2022_arm64/bin/Qt6Cored.dll)
