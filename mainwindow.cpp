74f4dcfae0b000b7e35f29e13b384f7e150f5ded
celine <celine.choln@gmail.com>
2026-01-28 21:41:34 +0100

    Version Widget code

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,6 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
-
+#include <QMessageBox>
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -11,4 +11,20 @@ MainWindow::MainWindow(QWidget *parent)
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_BtnSauvegarder_clicked()
+{
+    // Récupérer le texte du widget Code
+    QString texte = ui->Code->text();
+    if (ui->Code->text().isEmpty())
+    {
+        QMessageBox::warning(this,
+                             "Saisi du code",
+                    "Vous devez saisir un code !!");
+        return; // on arrête la fonction
+    }
+
+    // Copier ce texte dans le widget DernierCodeSaisi
+    ui->DernierCodeSaisi->setText(texte);
 }
